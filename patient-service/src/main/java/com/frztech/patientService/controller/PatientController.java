d722193d79830d5ae4051f5958cd2a9c39bd6379
Faraaz Hussain <faraaz.husayn@gmail.com>
2026-06-17 16:26:08 +0530

    Added Patient controller

---
+++ patient-service/src/main/java/com/frztech/patientService/controller/PatientController.java
@@ -0,0 +1,28 @@
+package com.frztech.patientService.controller;
+
+import com.frztech.patientService.dto.PatientResponseDTO;
+import com.frztech.patientService.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")  //all requests having url: localhost:4000/patients will be handled by this controller
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+
+        return ResponseEntity.ok().body(patients);
+    }
+}
